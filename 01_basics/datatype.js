79cefdd71d1f36c995a1984c8ca02e68a6377764
Aditya Maddheshiya <adityamaddhesiya143@gmail.com>
2025-02-27 12:38:08 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all jscode as newer version
+
+let name = "Addy"
+let age = 22
+let isloggedIn = true
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true/false
+// null => standalong value
+// undefined => means abhi value assign nhi hui h
+// symbol => unique
+
+// object
+
+console.log(typeof "Raj");
+
+// undefine aur null ka vale same wahi aata h
