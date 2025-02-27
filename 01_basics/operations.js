e838e4b2dec1e5a9662697dfe0d735b6c43bac1f
Aditya Maddheshiya <adityamaddhesiya143@gmail.com>
2025-02-27 13:12:00 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+let score = "addy"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(typeof(valueInNumber));
+
+// "55" => 55
+// "55abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = "addy"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+//1 => true;
+// "" => false
+// "addy" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
